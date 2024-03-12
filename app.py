ec826c6a6300b0a93d5c1ca53af9acb13a1ce343
Anna4931 <aniketpatel3172@gmail.com>
2024-03-12 10:16:15 +0530

    app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from dash import Dash, html, dcc, Input, Output, State
-import yfinance as yf
+from yfinance_data import Yfinance
 import pandas as pd
 
 app = Dash(__name__)
@@ -9,6 +9,7 @@ app.layout = html.Div([
         dcc.RadioItems(
             id='radio-items',
             options=[
+                {'label': 'International', 'value': ''},
                 {'label': 'NSC', 'value': '.NS'},
                 {'label': 'BSC', 'value': '.Bo'}
             ],
@@ -21,7 +22,18 @@ app.layout = html.Div([
     html.Div(
         children=[
             dcc.Dropdown(
-                options=['1D', '5D', '1M', '3M', '6M', '1Y', '2Y', '5Y', 'Max'],
+                id='period',
+                options=[
+                    {'label':'1D','value':'1D'},
+                    {'label':'5D','value':'5D'},
+                    {'label':'1M','value':'1mo'},
+                    {'label':'3M','value':'3mo'},
+                    {'label':'6M','value':'6mo'},
+                    {'label':'1Y','value':'1yr'},
+                    {'label':'2Y','value':'2yr'},
+                    {'label':'5Y','value':'5yr'},
+                    {'label':'Max','value':'Max'},
+                ],
                 value='1D',
                 style={'width': '80px'} 
             ),
@@ -32,6 +44,7 @@ app.layout = html.Div([
         ], 
         style={'display': 'flex', 'justifyContent': 'center', 'paddingTop': '20px','paddingBottom': '20px'}
     ),
+    
     html.Div(id='output-container-button')
 ],
     style={'backgroundColor': '#CFD8DC'}  
@@ -42,17 +55,59 @@ app.layout = html.Div([
     Output('output-container-button', 'children'),
     [Input('submit-val', 'n_clicks')],
     [State('text-input', 'value'),
-     State('radio-items', 'value')]
+     State('radio-items', 'value'),
+     State('period', 'value')]
 )
-def stock_info(n_clicks, value, radio_value):
+def stock(n_clicks, value, radio_value, period):
     if n_clicks:
         full_stock_name = value + radio_value
-        stock = yf.Ticker(full_stock_name)
-        info = stock.info
-        one_year = stock.history(period="1yr")
-        he = one_year.head()
+        yf_data = Yfinance(full_stock_name)  
+        info = yf_data.stock_info()
 
-        return f"Company Name:  {info['longName']},   Sector: {info['sector']},   Exchange: {info['exchange']} "
+        table_rows = [html.Tr([html.Th(key), html.Td(str(value))]) for key, value in info.items()]
+
+        data = yf_data.fetch_stock_data(period=period, interval='1d')
+        
+        info_table = html.Div([
+            html.H3("Info Table", style={'textAlign': 'center'}),  
+            html.Table(table_rows)
+        ])
+        
+        data_table = html.Div([
+            html.H3("Data Table", style={'textAlign': 'center'}),  
+            html.Table([
+                html.Thead(
+                    [html.Th(col) for col in data.columns]
+                ),
+                html.Tbody([
+                    html.Tr([
+                        html.Td(data.iloc[i][col]) 
+                        for col in data.columns
+                    ]) for i in range(len(data))
+                ])
+            ])
+        ])
+        
+        earnings = yf_data.stock_earning_date()
+        
+        earnings_df = pd.DataFrame.from_dict(earnings)
+        
+        earnings_table = html.Div([
+            html.H3("Earnings Table", style={'textAlign': 'center'}),  
+            html.Table([
+                html.Thead(
+                    html.Tr([html.Th(col) for col in earnings_df.columns])
+                ),
+                html.Tbody([
+                    html.Tr([
+                        html.Td(earnings_df.iloc[i][col]) for col in earnings_df.columns
+                    ]) for i in range(len(earnings_df))
+                ])
+            ])
+        ])
+        
+        return [info_table, data_table, earnings_table]
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
