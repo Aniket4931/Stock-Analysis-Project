b04734de555ee63c0866939d592a75c7c05ed64e
Anna4931 <aniketpatel3172@gmail.com>
2024-04-05 16:44:51 +0530

    talib

--- ta_lib_utility.py
+++ ta_lib_utility.py
@@ -2,14 +2,15 @@ import talib
 import pandas as pd
 import numpy as np
 import yfinance as yf
-
+from candles import candle_patterns
 
 class Talib:
     @staticmethod
-    def calculate_rsi(data, column_name='Close', periods=14):
+    def calculate_rsi(data, column_name='Close', periods=14,start_index=14):
         close_prices = data[column_name]
         rsi = talib.RSI(close_prices, timeperiod=periods)
         return rsi
+
 
     @staticmethod
     def calculate_macd(data, fast_period=12, slow_period=26, signal_period=9):
@@ -28,7 +29,7 @@ class Talib:
     def calculate_bollinger_bands_width(data, period=20, nbdevup=2, nbdevdn=2):
         upper, middle, lower = talib.BBANDS(data['Close'], timeperiod=period, nbdevup=nbdevup, nbdevdn=nbdevdn)
         bb_width = (upper - lower) / middle
-        data['BB Width'] = bb_width
+        data['BB_Width'] = bb_width
         return data
     
     @staticmethod
@@ -36,16 +37,12 @@ class Talib:
         high = data[high_col].values
         low = data[low_col].values
         close = data[close_col].values
-
         plus_di = talib.PLUS_DI(high, low, close, timeperiod=period)
         minus_di = talib.MINUS_DI(high, low, close, timeperiod=period)
-
         adx = talib.ADX(high, low, close, timeperiod=period)
-
         data['Plus DI'] = plus_di
         data['Minus DI'] = minus_di
         data['ADX'] = adx
-
         return data
 
 
@@ -69,4 +66,23 @@ class Talib:
 
         return data
     
+    
+    
+    def handle_candle_pattern(data):
+        for pattern_name, pattern_data in candle_patterns.items():
+            pattern_function_name = pattern_data['function']
+            pattern_description = pattern_data['description']
+            pattern_function = getattr(talib, pattern_function_name)
+            result = pattern_function(data['Open'], data['High'], data['Low'], data['Close'])
+            result_filtered = np.where(result != 0, result, np.nan)
+            data[pattern_name + '_result'] = result_filtered
+            data[pattern_name + 'description'] = pattern_description  
+
+        data.dropna(how='all', inplace=True)
+
+        return data
+
+
+
+
 
